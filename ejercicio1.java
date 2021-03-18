3fd440f21a96b0243ee6fc45f9341f7c59c1e970
AldoEuan <Euan21132@gmail.com>
2021-03-18 12:04:21 -0600

    cambios ejercicio 1

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,5 +1,27 @@
-public class ejercicio1 {
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class ejercicio1 extends Frame {
+    // constructor para ventana
+    private static final long serialVersionUID = 1L;
+
+    public ejercicio1() {
+        this.setSize(300, 300);
+        this.setVisible(true);
+
+    }
+
+    // clase main principal
     public static void main(String[] args) {
-        System.out.println("hola");
+
+        ejercicio1 ventana = new ejercicio1();
+        ventana.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+
+            }
+        });
+
     }
 }
